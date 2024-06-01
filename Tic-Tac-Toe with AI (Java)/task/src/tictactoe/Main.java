c272fd9835c0a708c8e6eda5709af87551a976e0
Rodrigo Suguimoto <rodrigo.suguimoto@10903-MB.local>
2024-06-01 11:25:55 -0300

    Looping input command

--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
@@ -14,36 +14,39 @@ public class Main {
             gameMenu.processUserCommand(userCommand);
             exitGame = gameMenu.isExitGame();
 
+            if (!exitGame) {
+                TicTacToe ticTacToe = new TicTacToe();
+                ticTacToe.printTicTacToe();
+
+                boolean isGameOver = ticTacToe.isGameOver();
+                while (!isGameOver) {
+                    if (ticTacToe.isXorO() == 'X') {
+                        String isSuccessfulMove;
+                        do {
+                            System.out.println("Enter the coordinates:");
+                            String coordinates = scanner.nextLine();
+                            isSuccessfulMove = ticTacToe.placeCell(coordinates);
+                        } while (isSuccessfulMove.equals("UNSUCCESSFUL"));
+                    }
+                    ticTacToe.printTicTacToe(); // Prints the TicTacToe after a successful move
+                    ticTacToe.doesTheGameHaveAWinner(); // Verify whether there's a winner or it's a draw
+                    isGameOver = ticTacToe.isGameOver(); // Verify if game is over
+
+                    if (ticTacToe.isXorO() == 'O' && !isGameOver) {
+                        System.out.println("Making move level \"easy\"");
+                        int[] randomCoordinate = AIPlayer.getRandomCoordinates(ticTacToe.getTicTacToe());
+                        String coordinates = String.format("%s %s", randomCoordinate[0], randomCoordinate[1]);
+                        ticTacToe.placeCell(coordinates);
+
+                        ticTacToe.printTicTacToe(); // Prints the TicTacToe after a successful move
+                        ticTacToe.doesTheGameHaveAWinner(); // Verify whether there's a winner or it's a draw
+                        isGameOver = ticTacToe.isGameOver(); // Verify if game is over
+                    }
+                }
+            }
+
         } while (!exitGame);
 
-        TicTacToe ticTacToe = new TicTacToe();
-        ticTacToe.printTicTacToe();
-
-        boolean isGameOver = ticTacToe.isGameOver();
-        while (!isGameOver) {
-            if (ticTacToe.isXorO() == 'X') {
-                String isSuccessfulMove;
-                do {
-                    System.out.println("Enter the coordinates:");
-                    String coordinates = scanner.nextLine();
-                    isSuccessfulMove = ticTacToe.placeCell(coordinates);
-                } while (isSuccessfulMove.equals("UNSUCCESSFUL"));
-            }
-            ticTacToe.printTicTacToe(); // Prints the TicTacToe after a successful move
-            ticTacToe.doesTheGameHaveAWinner(); // Verify whether there's a winner or it's a draw
-            isGameOver = ticTacToe.isGameOver(); // Verify if game is over
-
-            if (ticTacToe.isXorO() == 'O' && !isGameOver) {
-                System.out.println("Making move level \"easy\"");
-                int[] randomCoordinate = AIPlayer.getRandomCoordinates(ticTacToe.getTicTacToe());
-                String coordinates = String.format("%s %s", randomCoordinate[0], randomCoordinate[1]);
-                ticTacToe.placeCell(coordinates);
-
-                ticTacToe.printTicTacToe(); // Prints the TicTacToe after a successful move
-                ticTacToe.doesTheGameHaveAWinner(); // Verify whether there's a winner or it's a draw
-                isGameOver = ticTacToe.isGameOver(); // Verify if game is over
-            }
-        }
     }
 
 }
